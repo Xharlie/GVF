c3621dcb14a79da9bf90b1b0c0037b892f9c379a
xharlie <charlie.xu007@yahoo.com>
2020-05-17 16:47:36 -0700

    pp

--- train/train_gvf_decoder.py
+++ train/train_gvf_decoder.py
@@ -74,7 +74,8 @@ parser.add_argument('--manifold', action='store_true')
 parser.add_argument('--lossw', nargs='+', action='store', default=[0.0, 1.0, 0.0, 0.0, 1.0, 0.0], help="xyz, locnorm, locsqrnorm, dist, dirct, drct_abs")
 parser.add_argument('--distlimit', nargs='+', action='store', type=str, default=[1.0, 0.9, 0.9, 0.8, 0.8, 0.7, 0.7, 0.6, 0.6, 0.5, 0.5, 0.4, 0.4, 0.3, 0.3, 0.2, 0.2, 0.18, 0.18, 0.16, 0.16, 0.14, 0.14, 0.12, 0.12, 0.1, 0.1, 0.08, 0.08, 0.06, 0.06, 0.05, 0.05, 0.04, 0.04, 0.03, 0.03, 0.02, 0.02, 0.01, 0.01, -0.01])
 parser.add_argument('--surfrange', nargs='+', action='store', default=[0.0, 0.15], help="lower bound, upperbound")
-parser.add_argument('--act_lst', nargs='+', action='store', default=["relu","relu","relu","relu"], help="lower bound, upperbound")
+parser.add_argument('--act_lst', nargs='+', action='store', default=["relu","relu","relu","relu"])
+parser.add_argument('--channel_size', nargs='+', action='store', default=[256,256,256,128,128])
 
 
 
@@ -82,6 +83,7 @@ FLAGS = parser.parse_args()
 FLAGS.lossw = [float(i) for i in FLAGS.lossw]
 FLAGS.distlimit = [float(i) for i in FLAGS.distlimit]
 FLAGS.surfrange = [float(i) for i in FLAGS.surfrange]
+FLAGS.channel_size = [int(i) for i in FLAGS.channel_size]
 
 print(FLAGS)
 if FLAGS.res == 0:
